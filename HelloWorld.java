0d472d0013073b12b0c228afa24e257eb9f294e6
Benedikt Koch <51399886+benediktkoch62@users.noreply.github.com>
2019-06-05 17:00:47 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,13 @@
 public class HelloWorld {
 
+    private myMethod(String str) {
+        return (str);
+    }
+        
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World again");
+        
+        System.out.println("Hello " + args[0]);
     }
 
 }
